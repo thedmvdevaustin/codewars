8997611b3dbbba7319898bbb7468e5d9fe424520
thedmvdevaustin <thedmvdevaustin@gmail.com>
2023-09-10 12:37:34 -0400

    first solution to Simple Remove Duplicates

---
+++ 7kyu/simple-remove-duplicates.js
@@ -0,0 +1,24 @@
+// DESCRIPTION:
+// Remove the duplicates from a list of integers, keeping the last ( rightmost ) occurrence of each element.
+
+// Example:
+// For input: [3, 4, 4, 3, 6, 3]
+
+// remove the 3 at index 0
+// remove the 4 at index 1
+// remove the 3 at index 3
+// Expected output: [4, 6, 3]
+
+// More examples can be found in the test cases.
+
+// Good luck!
+
+//My solution
+function solve(arr) {
+    let ans = []
+    for (let i = arr.length-1; i >=0; i--){
+      if (!ans.includes(arr[i])) ans.push(arr[i])
+      
+    }
+    return ans.reverse()
+  }
