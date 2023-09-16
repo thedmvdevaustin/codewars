8e15f6a65e7fd11b61d3cc9aa7d8437ada79c981
thedmvdevaustin <thedmvdevaustin@gmail.com>
2023-09-16 12:38:13 -0400

    first solution to Grasshopper - Function syntax debugging

---
+++ 8kyu/grasshopper-function-syntax-debugging.js
@@ -0,0 +1,13 @@
+// DESCRIPTION:
+// Grasshopper - Function syntax debugging
+// A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them.
+
+//My solution
+//function starts off as this
+// function main [verb, noun]
+//   return verb + noun
+// }
+//in order for the function to run it needed to have this syntax
+function main (verb, noun){
+  return verb + noun
+}
