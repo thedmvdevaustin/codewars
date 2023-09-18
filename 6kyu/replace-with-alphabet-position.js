e51e05a940ea5684905fbe08098516d4e5f5f4b7
thedmvdevaustin <thedmvdevaustin@gmail.com>
2023-09-18 15:07:43 -0400

    first solution to Replace With Alphabet Position

---
+++ 6kyu/replace-with-alphabet-position.js
@@ -0,0 +1,20 @@
+// DESCRIPTION:
+// Welcome.
+
+// In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+
+// If anything in the text isn't a letter, ignore it and don't return it.
+
+// "a" = 1, "b" = 2, etc.
+
+// Example
+// alphabetPosition("The sunset sets at twelve o' clock.")
+// Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" ( as a string )
+
+//My solution
+function alphabetPosition(text) {
+    let alpha = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+    let ans = []
+    text.split("").forEach(x => alpha.includes(x.toLowerCase()) ? ans.push(1+alpha.indexOf(x.toLowerCase())) : 0)
+    return ans.join(" ")
+  }
