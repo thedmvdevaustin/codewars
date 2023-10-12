08e0ae2c7edd309a91173eec69f5919a7dc9d328
thedmvdevaustin <thedmvdevaustin@gmail.com>
2023-10-12 19:33:21 -0400

    First solution to Love vs friendship

---
+++ 7kyu/love-vs-friendship.js
@@ -0,0 +1,18 @@
+// DESCRIPTION:
+// If　a = 1, b = 2, c = 3 ... z = 26
+
+// Then l + o + v + e = 54
+
+// and f + r + i + e + n + d + s + h + i + p = 108
+
+// So friendship is twice as strong as love :-)
+
+// Your task is to write a function which calculates the value of a word based off the sum of the alphabet positions of its characters.
+
+// The input will always be made of only lowercase letters and will never be empty.
+
+//My solution
+function wordsToMarks(string){
+    let alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    return string.split("").reduce((acc,x) => acc+=alpha.indexOf(x)+1, 0)
+  }
