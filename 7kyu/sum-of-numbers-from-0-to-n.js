98c79aa24f37ea7c1ffcb7c4c7fcabed91effba7
thedmvdevaustin <thedmvdevaustin@gmail.com>
2024-05-06 19:35:32 -0400

    first solution to Sum of numbers from 0 to N

---
+++ 7kyu/sum-of-numbers-from-0-to-n.js
@@ -0,0 +1,45 @@
+// Description:
+
+// We want to generate a function that computes the series starting from 0 and ending until the given number.
+
+// Example:
+// Input:
+
+// > 6
+// Output:
+
+// 0+1+2+3+4+5+6 = 21
+
+// Input:
+
+// > -15
+// Output:
+
+// -15<0
+
+// Input:
+
+// > 0
+// Output:
+
+// 0=0
+
+//My solution 
+var SequenceSum = (function() {
+    function SequenceSum() {}
+  
+    SequenceSum.showSequence = function(count) {
+      if (count===0) return "0=0"
+      if (count < 0) return `${count}<0`
+      let string = ""
+      let sum = 0
+      for (let i = 0; i < count+1; i++){
+        sum+=i
+        string+=`${i}+`
+      }
+      return string.slice(0, string.length-1) + " = " + `${sum}`
+    };
+  
+    return SequenceSum;
+  
+  })();
